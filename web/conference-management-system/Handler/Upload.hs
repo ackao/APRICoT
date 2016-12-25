2d56517c28f8cbb95ace02ac84c4b043b1682d14
Jordan Brown <jmbrown@andrew.cmu.edu>
2016-12-24 21:48:35 -0500

    Mark conflicts on upload
    Allows the user to mark conflicting reviewers when they add a paper.

--- web/conference-management-system/Handler/Upload.hs
+++ web/conference-management-system/Handler/Upload.hs
@@ -1,6 +1,7 @@
 module Handler.Upload where
 
 import Import
+import DB
 import Text.Julius
 import qualified Data.ByteString as S
 import qualified Data.ByteString.Lazy as L
@@ -13,19 +14,22 @@ data FileForm = FileForm
     , title    :: Text
     , authors  :: [Text]
     , abstract :: Textarea
+    , conflicts :: [Key User]
     }
 
 postUploadR :: Handler Html
 postUploadR = do
-    ((result, _), _) <- runFormPost uploadForm
+    reviewerOpts <- reviewers
+    ((result, _), _) <- runFormPost $ uploadForm reviewerOpts
     case result of
-        FormSuccess (FileForm fi title authors abstract) -> do
+        FormSuccess (FileForm fi title authors abstract conflicts) -> do
             fileBytes <- runResourceT $ fileSource fi $$ sinkLbs
             (uid, _) <- requireAuthPair
             paperId <- runDB $ insert $ Paper uid (fileName fi)
                             title (unTextarea abstract)
                                  (S.pack . L.unpack $ fileBytes)
             _ <- runDB $ mapM (\author -> insert_ $ Author author paperId) authors
+            _ <- runDB $ mapM (\conflict -> insert_ $ Conflict paperId conflict) conflicts
             setMessage "PDF saved"
             redirect HomeR
         _ -> do
@@ -34,16 +38,24 @@ postUploadR = do
 
 getUploadR :: Handler Html
 getUploadR = do
-    (formWidget, formEnctype) <- generateFormPost uploadForm
+    reviewerOpts <- reviewers
+    (formWidget, formEnctype) <- generateFormPost $ uploadForm reviewerOpts
     defaultLayout $ do
         $(widgetFile "upload")
 
-uploadForm :: Form FileForm
-uploadForm = renderBootstrap3 BootstrapBasicForm $ FileForm
+uploadForm :: [(Text, Key User)] -> Form FileForm
+uploadForm reviewerOpts = renderBootstrap3 BootstrapBasicForm $ FileForm
     <$> fileAFormReq "Choose a file"
     <*> areq textField "Paper Title" Nothing
     <*> areq authorsField "Authors" Nothing
     <*> areq textareaField "Abstract" Nothing
+    <*> areq (checkboxesFieldList reviewerOpts) "Conflicts" Nothing
+
+reviewers :: Handler [(Text, Key User)]
+reviewers = do
+    reviewerEnts <- getReviewers
+    return $ map (\(Entity uid user) -> (userUsername user, uid)) reviewerEnts
+
 
 authorsField :: Field Handler [Text]
 authorsField = Field
